d1bea974440bf05781bf77023a1890b84f00ff39
SamGree green <sam.green7777@yahoo.com>
2024-05-22 19:24:54 +0000

    Create questions in script.js file

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+const question = [
+    {
+        question: "What year was the United Nations established?",
+        answer: [
+            { text: 1946, correct: false},
+            { text: 1951, correct: false},
+            { text: 1945, correct: true},
+            { text: 1948, correct: false},
+        ]
+    },
+    {
+        question: "What country has the highest life expectancy?",
+        answer: [
+            { text: "Japan", correct: false},
+            { text: "Sweden", correct: false},
+            { text: "Ireland", correct: false},
+            { text: "Hong Kong", correct: true},
+        ]
+    },
+    {
+        question: "How many elements are in the periodic table?",
+        answer: [
+            { text: 108, correct: false},
+            { text: 118, correct: true},
+            { text: 181, correct: false},
+            { text: 98, correct: false},
+        ] 
+    },
+    {
+        question: "Aureolin is a shade of what color?",
+        answer: [
+            { text: "Yellow", correct: true},
+            { text: "Red", correct: false},
+            { text: "Blue", correct: false},
+            { text: "Orange", correct: false},
+        ]
+    },
+
+]
