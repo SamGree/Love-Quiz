052ecb74e30ce99e527050f7029e92dceb76d40e
SamGree green <sam.green7777@yahoo.com>
2024-05-23 11:37:31 +0000

    change answer to answers and questions as well

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,38 @@
-
-
-const question = [
+const questions = [
     {
         question: "What year was the United Nations established?",
-        answer: [
-            { text: "1946", correct: false},
-            { text: "1951", correct: false},
-            { text: "1945", correct: true},
-            { text: "1948", correct: false},
+        answers: [
+            { text: "1946", correct: false },
+            { text: "1951", correct: false },
+            { text: "1945", correct: true },
+            { text: "1948", correct: false },
         ]
     },
     {
         question: "What country has the highest life expectancy?",
-        answer: [
-            { text: "Japan", correct: false},
-            { text: "Sweden", correct: false},
-            { text: "Ireland", correct: false},
-            { text: "Hong Kong", correct: true},
+        answers: [
+            { text: "Japan", correct: false },
+            { text: "Sweden", correct: false },
+            { text: "Ireland", correct: false },
+            { text: "Hong Kong", correct: true },
         ]
     },
     {
         question: "How many elements are in the periodic table?",
-        answer: [
-            { text: 108, correct: false},
-            { text: 118, correct: true},
-            { text: 181, correct: false},
-            { text: 98, correct: false},
-        ] 
+        answers: [
+            { text: "108", correct: false },
+            { text: "118", correct: true },
+            { text: "181", correct: false },
+            { text: "98", correct: false },
+        ]
     },
     {
         question: "Aureolin is a shade of what color?",
-        answer: [
-            { text: "Yellow", correct: true},
-            { text: "Red", correct: false},
-            { text: "Blue", correct: false},
-            { text: "Orange", correct: false},
+        answers: [
+            { text: "Yellow", correct: true },
+            { text: "Red", correct: false },
+            { text: "Blue", correct: false },
+            { text: "Orange", correct: false },
         ]
     },
 ];
@@ -47,55 +45,53 @@ let currentQuestionIndex = 0;
 let score = 0;
 
 //create function for restart index and score to 0
-function startQuiz(){
+function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
-    //this for go to the next question
     nextButton.innerHTML = "Next";
     showQuestion();
 }
-
 //create function for show question 
 function showQuestion() {
     resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.
-    question;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
- //add code to display the answers
- currentQuestion.answers.forEach(answer => {
-    const button = document.createElement("button");
-    button.innerHTML = answer.text;
-    button.classList.add("btn");
-    answerButtons.appendChild(button);
-    if (answer.correct){
-        button.dataset.correct = answer.correct;
-    }
-    button.addEventListener("click", selectAnswer);
+
+     //add code to display the answers
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
     });
 }
+
 // Code for remove all previous answer
-function resetState(){
+function resetState() {
     nextButton.style.display = "none";
-    while(answerButtons.firstChild){
+    while (answerButtons.firstChild) {
         answerButtons.removeChild(answerButtons.firstChild);
     }
-
 }
 
 //Create the Function to select the answer
-
-function selectAnswer(e){
+function selectAnswer(e) {
     const selectedBtn = e.target;
     const isCorrect = selectedBtn.dataset.correct === "true";
-    if (isCorrect){
+    if (isCorrect) {
         selectedBtn.classList.add("correct");
+        score++;
     } else {
         selectedBtn.classList.add("incorrect");
     }
     Array.from(answerButtons.children).forEach(button => {
-        if(button.dataset.correct === "true"){
+        if (button.dataset.correct === "true") {
             button.classList.add("correct");
         }
         button.disabled = true;
@@ -103,6 +99,30 @@ function selectAnswer(e){
     nextButton.style.display = "block";
 }
 
+//Define the function for score
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+//Create function for next button
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+});
+
 document.addEventListener("DOMContentLoaded", startQuiz);
-
-
